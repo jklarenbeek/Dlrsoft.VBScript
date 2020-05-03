27b80bc3f8a91ff2ebef76392542200603979a6b
circles-arrows <31648959+circles-arrows@users.noreply.github.com>
2020-05-04 07:12:40 +0800

    Asp Classic Core PoC working

--- examples/netcoreapp3.1/AspClassicCoreWeb/Startup.cs
+++ examples/netcoreapp3.1/AspClassicCoreWeb/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspClassicCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Session;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +19,16 @@ namespace AspClassicCoreWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataProtection();
+            services.AddDistributedMemoryCache();
+            services.AddSession(o =>
+            {
+                o.IdleTimeout = new TimeSpan(0, 30, 0);
+                o.IOTimeout = new TimeSpan(0, 0, 0, 5);
+                o.Cookie.HttpOnly = true;
+                o.Cookie.IsEssential = true;
+            });
+                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +39,15 @@ namespace AspClassicCoreWeb
                 app.UseDeveloperExceptionPage();
             }
 
+            // serve htm & html files
+            app.UseStaticFiles();
+
+
+            // serve classic asp core
+            app.UseSession();
+            app.UseAspClassic();
+
+            // other routing shit cool developers like...
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
